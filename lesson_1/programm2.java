c350969b80a0529c7cb43c5f0631b858e575e90e
Sh_Konstantin <743442@mail.ru>
2023-03-15 23:14:32 +0300

    les1

---
+++ lesson_1/programm2.java
@@ -0,0 +1,12 @@
+package lesson_1;
+public class programm2 {
+    public static void main(String[] args) {
+        float f = 123.3456f;
+        System.out.println(f);
+        System.out.println(getType(f));
+    }
+
+    static String getType (Object o) {
+        return o.getClass().getSimpleName();
+    }
+}
